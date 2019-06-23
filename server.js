03da9b4a32f26e72a5560f9d6c3478f76b268759
Anna Mendoza <annamendoza.curve@gmail.com>
2019-06-23 11:41:39 -0700

    updated response to check nodemon is working

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-const express = require('express')
-const morgan = require('morgan')
+require('dotenv').config()
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
 
-const app = express()
+console.log(process.env.API_TOKEN);
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
-app.use((req, res) => {
-  res.send('Hey there, world!')
+app.use((req,res) =>{
+	res.send("hey there!");
 })
 
-const PORT = 8000
+app.use(function validateBearerToken(req, res, next) {
+	console.log('validate bearer token middleware')
+	debugger
+	// move to the next middleware
+	next()
+})
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res){
+  res.json(validTypes)
+}
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  res.send('Hello, Pokemon!')
+}
+app.get('/pokemon', handleGetPokemon);
+
+const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
