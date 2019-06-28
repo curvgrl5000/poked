399b2e525fcd76893151b39b988728cc4dcc70a5
Anna Mendoza <annamendoza.curve@gmail.com>
2019-06-27 18:28:36 -0700

    updated for roduction deployment on Heroku

--- server.js
+++ server.js
@@ -1,43 +1,67 @@
-require('dotenv').config();
+require('dotenv').config(); // the .config method reads the .env file each time you start the server 
 const express = require('express');
 const morgan = require('morgan');
-
-console.log(process.env.API_TOKEN);
-
+const helmet = require('helmet'); // hides info! https://github.com/helmetjs/helmet#how-it-works
+const cors = require('cors');
+const POKEDEX = require('./pokedex.json');
 const app = express();
+const morganSetting = process.env.NODE_ENV === 'production' 'tiny' : 'common';
 
-app.use(morgan('dev')); // 'use' is universally setting up the app to use morgan calling the 'dev' argument
-
-// app.get('/', (req,res) => {
-// 	res.send("hey there! go to the '/types' endpoint!");
-// });
+app.use(morgan('morganSetting')); // 'use' is universally setting up the app to use morgan calling the 'dev' argument
+app.use(cors()); // for cross origin pre-flight request from a different port
+app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next) { // 'use' is univerally or globally setting up the middleware to be used with each RESTFUL action
 	const apiToken = process.env.API_TOKEN;
 	const authToken = req.get('Authorization');
-
-	console.log('validate bearer token middleware')
-	
 	if( !authToken || authToken.split(' ')[1] !== apiToken){
 		return res.status(401).json({ error: 'Unauthorized request' })
 	}
 	// move to the next middleware with next call:
-	next()
-})
+	next();
+});
+
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
 function handleGetTypes(req, res){
   res.json(validTypes)
 }
+
 app.get('/types', handleGetTypes);
 
 
 function handleGetPokemon(req, res) {
-  res.send('Hello, Pokemon!')
+  let response = POKEDEX.pokemon; // our data property 'pokemon'
+	const { name, type } = req.query; // the query string KEYS used: name and type
+
+	if(name) { // if there IS a key name
+    response = response.filter(pokemon => 
+    	pokemon.name.toLowerCase().includes( name.toLowerCase() )
+    )
+	}
+
+	if(type) { // if there IS a key type
+    response = response.filter(pokemon => 
+    	pokemon.type.includes( type )
+    )
+	}
+	res.json(response);
 }
+
 app.get('/pokemon', handleGetPokemon);
 
-const PORT = 8000;
+app.use( (error, req, res, next) => {
+	let response;
+	if(process.env.NODE_ENV === 'production') {
+		response = { error: { message: 'server error'}}
+	} else {
+		response = { error }
+	}
+	res.status(500).json(response)
+});
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
